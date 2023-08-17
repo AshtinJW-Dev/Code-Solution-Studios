ea2f02462a18804f44cc4fab8a19dcba1e51cef5
Ashtin Walter <113695788+AshtinJW-Dev@users.noreply.github.com>
2023-08-17 18:10:59 +0200

    php mailer testing
    remember to remove pass later

--- Website/Code Solution Studios/assets/php/mail.php
+++ Website/Code Solution Studios/assets/php/mail.php
@@ -1,19 +1,51 @@
 <?php
-//get data from form  
+//Import PHPMailer classes into the global namespace
+//These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+//Load Composer's autoloader
+require 'vendor/autoload.php';
+
+//Create an instance; passing `true` enables exceptions
+$mail = new PHPMailer(true);
 
 $name = $_POST['name'];
-$email= $_POST['email'];
-$telNumber= $_POST['tel-number'];
-$service= $_POST['service'];
-$plan= $_POST['plan'];
-$message= $_POST['message'];
-$to = "codesolutionstudios@gmail.com";
-$subject = "Mail From website";
-$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Number =" . $telNumber"\r\n Service =" . $service"\r\n Plan =" . $plan"\r\n Message =" . $message;
-$headers = "From: noreply@codesolutionstudios.co.za" . "\r\n"
-if($email!=NULL){
-    mail($to,$subject,$txt,$headers);
+$visitor_email = $_POST['email'];
+$message = $_POST['message'];
+
+try {
+    //Server settings
+                  //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'mail.codesolutionstudios.co.za';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'noreply@codesolutionstudios.co.za';                     //SMTP username
+    $mail->Password   = '_gvZ2E0.gx6U';                               //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+    //Recipients
+    $mail->setFrom($visitor_email, 'Mailer:');
+    $mail->addAddress('noreply@codesolutionstudios.co.za', 'Joe User');     //Add a recipient
+             //Name is optional
+    $mail->addReplyTo($visitor_email, 'Information');
+
+    //Attachments
+    //$mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+    //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+
+    //Content
+    
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = 'Here is the subject';
+    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+    $mail->AltBody = $message;
+    
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
 }
-//redirect
-header("Location:index.html");
-?>
